2262999f1fe084b8d90dc199fd099e8d7c16e862
Tufan Calisir <tufan.calisir@project-joker.com>
2025-07-12 01:01:10 +0200

    chore: setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,24 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { storyblok } from "@storyblok/astro";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
+import { loadEnv } from "vite";
+import mkcert from "vite-plugin-mkcert";
 
-// https://astro.build/config
-export default defineConfig({});
+const env = loadEnv("", process.cwd(), "STORYBLOK");
+
+export default defineConfig({
+  integrations: [
+    storyblok({
+      output: "static",
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {},
+      apiOptions: {
+        region: "eu",
+      },
+      bridge: true,
+    }),
+  ],
+  vite: {
+    plugins: [tailwindcss(), mkcert()],
+  },
+});
